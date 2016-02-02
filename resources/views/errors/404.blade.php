907ceb6aae610ca69131794073e92d83b9b80011
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-02-03 00:13:34 +0100

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,10 @@
+@extends('layouts.master')
+
+@section('body_classes', 'error404')
+
+@section('content')
 <section class="v-404">
     <h1>Whoops!</h1>
-    <a href="#" title="Home"><i class="fa fa-long-arrow-left"></i></a>
+    <a href="/" title="Home"><i class="fa fa-long-arrow-left"></i></a>
 </section>
+@endsection
