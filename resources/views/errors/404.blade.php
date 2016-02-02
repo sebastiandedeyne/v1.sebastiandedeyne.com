1ea40b344d26a20f6c6ca9e87e1a7403bfdc5418
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-02-03 00:15:25 +0100

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <section class="v-404">
     <h1>Whoops!</h1>
+    <p class="mb:1">A wild 404 error appeared.</p>
     <a href="/" title="Home"><i class="fa fa-long-arrow-left"></i></a>
 </section>
 @endsection
