b65893877999f0322e36b8d27163cd660b8bfb19
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-02-05 22:07:18 +0100

    Controller fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Foundation\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
