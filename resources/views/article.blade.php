48a353c6cd3064dd93b615207340c4ce1e689b17
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-07-07 12:22:41 +0200

    Utils to bem

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,20 +4,20 @@
 @section('meta', $article->description)
 
 @section('content')
-<main class=grid>
-    <div class=grid__row>
-        <header class="fw:xbold fs:20 col:red mb:1 mt:1/2">
-            <a href="{{ url('/') }}" class=logotype>Sebastian De Deyne</a>
+<main class="grid">
+    <div class="grid__row">
+        <header class="site-header">
+            <a href="{{ url('/') }}" class="site-header__logotype">Sebastian De Deyne</a>
         </header>
     </div>
-    <div class=grid__row>
-        <article class=v-article>
-            <header class="mb:1">
-                <h1 class="fs:33 fw:xbold mb:1/8">
+    <div class="grid__row">
+        <article class="v-article">
+            <header class="v-article__header">
+                <h1 class="v-article__header__title">
                     {{ $article->title }}
                 </h1>
                 @if($article->date)
-                <aside class="fs:14 col:text--lighter mb:1">
+                <aside class="v-article__header__meta">
                     Published {{ $article->date->format('j F Y') }}
                     @if($article->era)
                         — <em>{{ $article->era }}</em>
@@ -25,17 +25,17 @@
                 </aside>
                 @endif
             </header>
-            <section class=v-article__body>
+            <section class="v-article__body">
                 {!! $article->contents !!}
             </section>
             @if($article->commentable)
-            <section class="v-article__comments mt:2">
+            <section class="v-article__comments">
                 @include('partials.disqus')
             </section>
             @endif
-            <section class="v-article__footer fs:14 lh:150 mt:2">
-                <p class="mb:1/2">© {{ now()->format('Y') }} <a href="{{ url('/about') }}">Sebastian De Deyne</a> <span class="col:text--lighter fs:12">【ツ】</span></p>
-                <p class="mb:1">
+            <section class="v-article__footer">
+                <p class="v-article__footer__credits">© {{ now()->format('Y') }} <a href="{{ url('/about') }}">Sebastian De Deyne</a> <span class="col:text--lighter fs:12">【ツ】</span></p>
+                <p class="v-article__footer__about">
                     I'm a full-stack developer working at <a href="https://spatie.be" target="sebastiandedeyne.com">Spatie</a> in Antwerp, Belgium.
                     If you've got any comments, feedback or just want to chat you can get in touch via <a href="https://twitter.com/sebdedeyne" target="sebastiandedeyne.com">Twitter</a> or <a href="mailto:sebastiandedeyne@gmail.com">email</a>.
                 </p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,27 +6,27 @@
 <main class="v-home grid">
     <div class="grid__row">
         <div class="grid__col--3/4">
-            <header class="mb:1/4 fs:25 ls:06 tt:upper fw:bold">
+            <header class="v-home__title">
                 <h1>Sebastian De Deyne</h1>
             </header>
-            <section class="v-home__intro mb:3/2 fs:27 lh:125 ls:03">
+            <section class="v-home__intro">
                 <p>I'm a full-stack developer working at <a href="https://spatie.be" target="_blank">Spatie</a> in Antwerp, Belgium.</p>
             </section>
         </div>
     </div>
     <div class="grid__row">
-        <section class="v-home__toc mb:2 fs:17">
-            <h2 class="tt:upper fw:bold">Blog</h2>
-            <ul class="mb:1">
+        <section class="v-home__toc">
+            <h2 class="v-home__toc__title">Blog</h2>
+            <ul class="v-home__toc__list">
                 @foreach($posts as $post)
                     <li>
                         <a href="{{ $post->url }}">{{ $post->title }}</a>
-                        <aside class="d:i fs:14 o:1/2">{{ $post->date->format('m/Y') }}</aside>
+                        <aside class="v-home__toc__list__aside">{{ $post->date->format('m/Y') }}</aside>
                     </li>
                 @endforeach
             </ul>
-            <h2 class="tt:upper fw:bold">More</h2>
-            <ul class="mb:1">
+            <h2 class="v-home__toc__title">More</h2>
+            <ul class="v-home__toc__list">
                 @foreach($more as $item)
                     <li>
                         @if(isset($item[1]))
@@ -35,7 +35,7 @@
                             {{ $item[0] }}
                         @endif
                         @if(isset($item[2]))
-                            <aside class="d:i fs:14 o:1/2">{{ $item[2] }}</aside>
+                            <aside class="v-home__toc__list__aside">{{ $item[2] }}</aside>
                         @endif
                     </li>
                 @endforeach
@@ -43,7 +43,7 @@
         </section>
     </div>
     <div class="grid__row">
-        <footer class="v-home__footer fs:20">
+        <footer class="v-home__footer">
             <a href="https://twitter.com/sebdedeyne" class=btn target="_blank" title="Twitter">
                 <i class="fa fa-twitter"></i>
             </a>
