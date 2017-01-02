c4dc527795825c6815ebf77462c7c8b30981aba6
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-02 15:04:40 +0100

    Enable middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace], function ($router) {
+        $router->group(['namespace' => $this->namespace, 'middleware' => 'web'], function () {
             require app_path('Http/routes.php');
         });
     }
