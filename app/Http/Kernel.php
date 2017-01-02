9c50f0c0c175fc9436cd9a35c42b3c3441a7adb7
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-02 14:53:14 +0100

    Cache control

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,11 +24,12 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            //\App\Http\Middleware\EncryptCookies::class,
+            //\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            //\Illuminate\Session\Middleware\StartSession::class,
+            //\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            //\App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\CacheControl::class,
         ],
 
         'api' => [
