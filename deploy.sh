2336bd3f8ec859dcfa66a68725dad7abae154bc5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-20 09:31:47 +0100

    Scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 php artisan down
 
-# There's an issue that's causing yarn to modify the lock file on server
-git checkout yarn.lock 
 git pull origin master
 
 composer install --no-interaction --no-dev --prefer-dist
