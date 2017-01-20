e95b46b02fe8290948ae9e5c51c1bb54fee84a89
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-20 08:12:19 +0100

    Redesign

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,8 +10,19 @@
             About
         </h1>
         <p class="intro__text intro__text--small">
-            I'm a full-stack developer from Ghent, working at Spatie in Antwerp, where I mainly build stuff with Laravel and Vue. In my spare time I work on some (open source) side projects, and improve my cooking skills.
+            I'm a full-stack developer from Ghent, working at Spatie in Antwerp, where I mainly build stuff with Laravel and Vue.js. In my spare time I work on some (open source) side projects, and improve my cooking skills.
         </p>
+    </section>
+    <section class="textblock">
+        <h2 class="textblock__subtitle">Experience</h2>
+        <ul>
+            <li>Developer at Spatie, Antwerp (2015- <span class="shruggie">¯\_(ツ)_/¯</span>)</li>
+            <li>Freelance design & dev as 1/3 of Made By Monkey, Ghent (2012-2015)</li>
+            <li>Internship at SumoCoders, Ghent (2014)</li>
+            <li>Bachelor's degree in graphical and digital media<br> (Arteveldehogeschool 2015)</li>
+        </ul>
+        <h2 class="textblock__subtitle">Colophon</h2>
+        <p>This website is powered by <a href="https://laravel.com/" target="sebdd">Laravel</a> and served from <a href="https://www.digitalocean.com/" target="sebdd">Digital Ocean</a>. The source code is hosted on <a href="https://github.com/sebastiandedeyne/sebastiandedeyne.com" target="sebdd">Github</a>.</p>
     </section>
 </div>
 <footer class="footer">
