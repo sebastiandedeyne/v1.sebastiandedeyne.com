b6cd498f7133df82dae8dab58fe5bff072585112
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-23 22:28:28 +0100

    Error pages

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,12 +1,12 @@
-@extends('_layouts.master')
-
-@section('content')
-<section class="error">
-    <h1 class="error__title">
-        Be Right Back!
-    </h1>
-    <p class="error__message">
-        Just doing some quick maintenance.
-    </p>
-</section>
-@endsection
+@component('layouts.page', [
+    'title' => 'Be Right Back!',
+])
+    <section class="error">
+        <h1 class="error__title">
+            Be Right Back!
+        </h1>
+        <p class="error__message">
+            Just doing some quick maintenance.
+        </p>
+    </section>
+@endcomponent
--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -1,15 +1,15 @@
-@extends('_layouts.master')
-
-@section('content')
-<section class="error">
-    <h1 class="error__title">
-        {{ $title }}
-    </h1>
-    <p class="error__message">
-        {{ $message }}
-    </p>
-    <a href="{{ url('/') }}" class="button">
-        Go home
-    </a>
-</section>
-@endsection
+@component('layouts.page', [
+    'title' => $title,
+])
+    <section class="error">
+        <h1 class="error__title">
+            {{ $title }}
+        </h1>
+        <p class="error__message">
+            {{ $message }}
+        </p>
+        <a href="{{ url('/') }}" class="button">
+            Go home
+        </a>
+    </section>
+@endcomponent
