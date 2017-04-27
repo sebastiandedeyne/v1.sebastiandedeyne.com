f689369e2dd4e7f73209ace93962626f69f3c8e3
Sebastian De Deyne <sebastian@spatie.be>
2017-04-27 12:26:17 +0200

    Smoke tests

--- tests/Smoke/SmokeTest.php
+++ tests/Smoke/SmokeTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Tests\Smoke;
 
 class SmokeTest extends TestCase
 {
--- tests/Smoke/TestCase.php
+++ tests/Smoke/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Tests\Smoke;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\CreatesApplication;
