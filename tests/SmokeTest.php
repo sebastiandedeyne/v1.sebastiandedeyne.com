35348d000fbb8cc7fa3a009cacc41e61c74e00f2
Sebastian De Deyne <sebastian@spatie.be>
2017-04-27 12:25:12 +0200

    Smoke testsp

---
+++ tests/SmokeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+
+class SmokeTest extends TestCase
+{
+    public function test_it_displays_the_home_page()
+    {
+        $this->get('/')->assertStatus(200);
+    }
+
+    public function test_it_displays_the_about_page()
+    {
+        $this->get('/about')->assertStatus(200);
+    }
+
+    public function test_it_displays_the_open_source_page()
+    {
+        $this->get('/open-source')->assertStatus(200);
+    }
+
+    public function test_it_displays_a_post_page()
+    {
+        $this->get('/posts/2016/adventure-time-with-webpack')->assertStatus(200);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
