80f47b8cbaac5062364d63496db7170ec5a9b3f9
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-06-30 11:59:36 +0200

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,6 @@
 <footer class="footer">
-    © {{ carbon()->format('Y') }} Sebastian De Deyne
+    © {{ carbon()->format('Y') }}
+    <a href="{{ url('about') }}" class="footer__link -subtle">Sebastian De Deyne</a>
+    &#8226;
+    <a href="{{ url('rss') }}" class="footer__link">RSS</a>
 </footer>
