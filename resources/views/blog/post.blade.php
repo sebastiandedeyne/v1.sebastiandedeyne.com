0cce125b8ea4a20cda74e3f65c636d00dd2f528e
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-07-03 21:06:19 +0200

    Remove /edit/ from GitHub urls

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -50,7 +50,7 @@
                     <a class="post__footer__link -subtle" href="{{ url('about') }}">Sebastian De Deyne</a>
                 </p>
                 <p class="post__footer__p">
-                    If you've got any comments, feedback or just want to chat you can get in touch on <a class="post__footer__link" href="https://twitter.com/sebdedeyne" target="sebdd">Twitter</a> or via <a class="post__footer__link" href="mailto:sebastiandedeyne@gmail.com">e-mail</a>. If you catch a mistake or notice something that could be improved, feel free to <a class="post__footer__link" target="sebdd" href="https://github.com/sebastiandedeyne/sebastiandedeyne.com/edit/{{ config('app.branch') }}/content/{{ $post->slug }}.md">edit this post on GitHub</a>.
+                    If you've got any comments, feedback or just want to chat you can get in touch on <a class="post__footer__link" href="https://twitter.com/sebdedeyne" target="sebdd">Twitter</a> or via <a class="post__footer__link" href="mailto:sebastiandedeyne@gmail.com">e-mail</a>. If you catch a mistake or notice something that could be improved, feel free to <a class="post__footer__link" target="sebdd" href="https://github.com/sebastiandedeyne/sebastiandedeyne.com/{{ config('app.branch') }}/content/{{ $post->slug }}.md">edit this post on GitHub</a>.
                 </p>
             </div>
         </footer>
