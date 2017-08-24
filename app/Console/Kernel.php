4d2f4fe76b7ee396b83ff7dc538bdc567c235c52
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-08-24 10:46:33 +0200

    Unregister dd command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Spatie\ArtisanDd\DdCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +14,6 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\PostMakeCommand::class,
-        DdCommand::class,
     ];
 
     /**
