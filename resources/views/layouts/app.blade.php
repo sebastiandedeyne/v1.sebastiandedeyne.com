c51cb207d924171485ee9d52673d3bbb13407f9f
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-09-25 11:54:56 +0200

    Iphone X compat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
         <meta name="description" content="{{ $meta_description ?? 'I\'m a web developer working at Spatie in Antwerp, Belgium.' }}">
         <meta name="author" content="Sebastian De Deyne">
         @if(isset($canonical_url) && $canonical_url)
