1d8587d69f892a1b4822f3becda0c9c197144384
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-10-05 21:17:15 +0200

    Fix post edit link (closes #50)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -48,7 +48,7 @@
                     <a class="post__footer__link -subtle" href="{{ route('about') }}">Sebastian De Deyne</a>
                 </p>
                 <p class="post__footer__p">
-                    If you've got any comments, feedback or just want to chat you can get in touch on <a class="post__footer__link" href="https://twitter.com/sebdedeyne" target="sebdd">Twitter</a> or via <a class="post__footer__link" href="mailto:sebastiandedeyne@gmail.com">e-mail</a>. If you catch a mistake or notice something that could be improved, feel free to <a class="post__footer__link" target="sebdd" href="https://github.com/sebastiandedeyne/sebastiandedeyne.com/{{ config('app.branch') }}/content/{{ $post->slug }}.md">edit this post on GitHub</a>.
+                    If you've got any comments, feedback or just want to chat you can get in touch on <a class="post__footer__link" href="https://twitter.com/sebdedeyne" target="sebdd">Twitter</a> or via <a class="post__footer__link" href="mailto:sebastiandedeyne@gmail.com">e-mail</a>. If you catch a mistake or notice something that could be improved, feel free to <a class="post__footer__link" target="sebdd" href="https://github.com/sebastiandedeyne/sebastiandedeyne.com/edit/{{ config('app.branch') }}/content/posts/{{ $post->date->format('Y-m-d') }}.{{ $post->slug }}.md">edit this post on GitHub</a>.
                 </p>
             </div>
         </footer>
