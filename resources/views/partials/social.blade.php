99d46f24c15aad9b3069618901c451d719578fb6
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-03-07 12:39:40 +0100

    Fix odd flahy button behaviour

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,14 +1,16 @@
-@foreach([
-    ['sebdedeyne', 'twitter', 'Twitter', 'https://twitter.com/sebdedeyne'],
-    ['sebastiandedeyne', 'github', 'GitHub', 'https://github.com/sebastiandedeyne'],
-    ['sebastiandedeyne@gmail.com', 'email', 'E-mail', 'mailto:sebastiandedeyne@gmail.com'],
-] as [$name, $icon, $title, $url])
-    <a class="button -faded" href="{{ $url }}" target="sebdd">
-        <span class="button__icon">
-            <span class="icon" title="{{ $title }}">
-                {{ svg($icon) }}
+<div class="h-flex">
+    @foreach([
+        ['sebdedeyne', 'twitter', 'Twitter', 'https://twitter.com/sebdedeyne'],
+        ['sebastiandedeyne', 'github', 'GitHub', 'https://github.com/sebastiandedeyne'],
+        ['sebastiandedeyne@gmail.com', 'email', 'E-mail', 'mailto:sebastiandedeyne@gmail.com'],
+    ] as [$name, $icon, $title, $url])
+        <a class="button -faded" href="{{ $url }}" target="sebdd">
+            <span class="button__icon">
+                <span class="icon" title="{{ $title }}">
+                    {{ svg($icon) }}
+                </span>
             </span>
-        </span>
-        {{ $name }}
-    </a>
-@endforeach
+            {{ $name }}
+        </a>
+    @endforeach
+</div>
