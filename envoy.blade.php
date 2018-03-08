78f0784e585c9c4d0763810332e3e90d28bbe5ad
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-03-08 11:57:56 +0100

    Update Envoy script

--- envoy.blade.php
+++ envoy.blade.php
@@ -1,5 +1,6 @@
 @setup
     $dev = $dev ?? false;
+    $quick = $quick ?? false;
     $root = $dev ? '/home/forge/dev.sebastiandedeyne.com' : '/home/forge/sebastiandedeyne.com';
     $branch = $dev ? 'dev' : 'master';
     $releaseDir = "{$root}/releases/".date('Y-m-d-His');
@@ -8,36 +9,36 @@
 @servers(['web' => ['forge@sebastiandedeyne.com']])
 
 @task('deploy', ['on' => 'web'])
-    echo "Deploying new release: {{ $releaseDir }}"
+    @if($quick)
+        cd $(ls -dt {{ $root }}/releases/* | head -n 1)
+        git pull
 
-    mkdir -p releases
-	git clone -b {{ $branch }} git@github.com:sebastiandedeyne/sebastiandedeyne.com.git {{ $releaseDir }}
+        php artisan cache:clear
+        php artisan responsecache:flush
+        php artisan config:cache
+        php artisan route:cache
+    @else
+        echo "Deploying new release: {{ $releaseDir }}"
 
-    cd {{ $releaseDir }}
+        mkdir -p releases
+        git clone -b {{ $branch }} git@github.com:sebastiandedeyne/sebastiandedeyne.com.git {{ $releaseDir }}
 
-    composer install --no-dev --no-interaction --prefer-dist --quiet
-    yarn --frozen-lockfile --silent
-    yarn production
+        cd {{ $releaseDir }}
 
-    rm -f {{ $root }}/current
-	ln -s {{ $releaseDir }} {{ $root }}/current
-	ln -s {{ $releaseDir }}/../../.env {{ $releaseDir }}/.env
-	sudo service php7.1-fpm restart
+        composer install --no-dev --no-interaction --prefer-dist --quiet
+        yarn --frozen-lockfile --silent
+        yarn production
 
-    php artisan cache:clear
-    php artisan responsecache:flush
-    php artisan config:cache
-    php artisan route:cache
+        rm -f {{ $root }}/current
+        ln -s {{ $releaseDir }} {{ $root }}/current
+        ln -s {{ $releaseDir }}/../../.env {{ $releaseDir }}/.env
+        sudo service php7.1-fpm restart
 
-    ls -dt {{ $root }}/releases/* | tail -n +3 | xargs -d "\n" rm -rf;
-@endtask
+        php artisan cache:clear
+        php artisan responsecache:flush
+        php artisan config:cache
+        php artisan route:cache
 
-@task('quick-deploy', ['on' => 'web'])
-    cd $(ls -dt {{ $root }}/releases/* | head -n 1)
-    git pull
-
-    php artisan cache:clear
-    php artisan responsecache:flush
-    php artisan config:cache
-    php artisan route:cache
+        ls -dt {{ $root }}/releases/* | tail -n +3 | xargs -d "\n" rm -rf;
+    @endif
 @endtask
