6d5b199fea059bd8dc2ed210583f6452afc4fae6
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-05-12 23:11:36 +0200

    Prefetch extra fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,700,700i|IBM+Plex+Serif:400,400i,500,700" rel="stylesheet">
 
     <link rel="prefetch" href="{{ mix('js/highlight.js') }}" as="script">
+    <link rel="prefetch" href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono" as="style">
   </head>
   <body>
     <div class="flex flex-col min-h-screen">
