c327c6b96c3e9d90ae83bce98adeed1c45b4a345
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-05-12 23:15:29 +0200

    Revert

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\HtmlString;
 
+function inline_mix(string $path): HtmlString
+{
+    $contents = file_get_contents(
+        public_path(mix($path))
+    );
+
+    return new HtmlString($contents);
+}
+
 function svg($filename): HtmlString
 {
     $contents = @file_get_contents(
