ec92657409518041d2f0af496cd7bf4f89c67923
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-05-22 12:02:45 +0200

    Home update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,19 +1,19 @@
 @component('layouts.app')
-  <section class="introduction markup">
-    <p class="mb-4">
-      I'm a <em>web designer</em> and <em>developer</em> from Ghent,<br> employed at
-      <a href="https://spatie.be">Spatie</a> in Antwerp, Belgium.
-    </p>
-    <p class="mb-4">
-      I build <em>websites</em> and <em>applications</em> with <br>
-      JavaScript, PHP, HTML and CSS.
-    </p>
+  <section class="markup | font-serif mb-12 sm:mb-20 md:mb-32 sm:pt-20">
+    <div class="sm:text-lg sm:tracking-wide mb-8">
+      <p>
+        I'm a <em>web designer</em> and <em>developer</em> from Ghent,<br class="hidden md:inline"> employed at
+        <a href="https://spatie.be">Spatie</a> in Antwerp, Belgium.
+      </p>
+      <p>
+        I build <em>websites</em> and <em>applications</em> with <br class="hidden md:inline">
+        JavaScript, PHP, HTML and CSS.
+      </p>
+    </div>
     <p>
-      I <a href="https://twitter.com/sebdedeyne">tweet</a>,
+      I also <a href="https://twitter.com/sebdedeyne">tweet</a>,
       <a href="{{ route('posts') }}">blog</a>,
-      publish a <a href="{{ route('newsletter') }}">newsletter</a>,
-      and do talks
-      every now and then too.
+      and publish a <a href="{{ route('newsletter') }}">newsletter</a>.
     </p>
   </section>
 
