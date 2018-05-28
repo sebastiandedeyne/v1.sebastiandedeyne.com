30d4599369d47a62a5052887631da1cadf9eec1d
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-05-28 12:19:56 +0200

    Fix footer link

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <div class="py-3 border-t md:flex justify-between text-center">
       <p>
-        © 2018 <a href="{{ route('posts') }}">Sebastian De Deyne</a>
+        © 2018 <a href="{{ route('home') }}">Sebastian De Deyne</a>
       </p>
       <p>
         <a href="https://twitter.com/sebdedeyne">@sebdedeyne</a> · <a href="{{ route('feeds.main') }}">RSS</a>
