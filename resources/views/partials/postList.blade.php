9bb71bc1ed647bbe69c6946fb39e13e256cb7d2c
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-06-08 21:57:57 +0200

    Improve postList link

--- resources/views/partials/postList.blade.php
+++ resources/views/partials/postList.blade.php
@@ -5,15 +5,13 @@
   <ul>
     @foreach($posts as $post)
       <li class="mt-4">
-        <a href="{{ $post->url }}">
-          <strong class="block font-normal leading-tight">
-            {{ $post->title }}
-          </strong>
-          <p class="text-xs text-grey leading-tight sm:mt-1">
-            <time datetime="{{ $post->date->format('Y-m-d') }}">
-              {{ $post->date->format('F jS, Y') }}
-            </time>
-          </p>
+        <a href="{{ $post->url }}" class="block font-normal leading-tight">
+          {{ $post->title }}
+        </a>
+        <a href="{{ $post->url }}" class="block text-xs text-grey leading-tight sm:mt-1">
+          <time datetime="{{ $post->date->format('Y-m-d') }}">
+            {{ $post->date->format('F jS, Y') }}
+          </time>
         </a>
       </li>
     @endforeach
