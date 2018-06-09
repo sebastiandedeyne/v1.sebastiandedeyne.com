37fa10296e7972b937cc09ef5fe0d242fb12b7bd
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-06-09 17:13:26 +0200

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('posts', 'PostsController@index')->name('posts');
 Route::get('posts/{year}/{slug}', 'PostsController@redirectOldPost');
 
 Route::get('{post}', 'PostsController@show')->name('posts.show');
-Route::get('{post}/og-image', 'PostsController@igImage');
+Route::get('{post}/og-image', 'PostsController@ogImage');
 
 Route::domain('growingthestack.io')->group(function () {
     Route::redirect('{url}', 'sebastiandedeyne.com/newsletter')->where('url', '(.*)');
