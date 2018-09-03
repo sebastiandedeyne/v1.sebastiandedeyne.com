df65cddb61d828ea5c8679acda6ba76461c85718
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-09-03 18:10:13 +0200

    Fix CacheControl

--- app/Http/Middleware/CacheControl.php
+++ app/Http/Middleware/CacheControl.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CacheControl
 {
@@ -11,9 +12,19 @@ class CacheControl
         /** @var \Illuminate\Http\Response $response */
         $response = $next($request);
 
-        if (app()->environment('production')) {
-            $response->header('Cache-Control', 'max-age=1000, public');
+        if (! app()->environment('production')) {
+            return $response;
         }
+
+        if (! Auth::check()) {
+            return $response;
+        }
+
+        if ($request->method() !== 'GET') {
+            return $response;
+        }
+
+        $response->header->add('Cache-Control', 'max-age=1000, public');
 
         return $response;
     }
