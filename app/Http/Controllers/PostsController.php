f2da6f9e05b11ded05601a40d98d4e47c0533bbc
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-11-04 10:01:38 +0100

    Fix posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,18 +7,6 @@ use Spatie\Sheets\Sheets;
 
 class PostsController
 {
-    public function index(Sheets $sheets)
-    {
-        $posts = $sheets->collection('posts')->all()
-            ->sortByDesc(function ($post) {
-                return $post->date;
-            });
-
-        return view('posts.index', [
-            'posts' => $posts,
-        ]);
-    }
-
     public function show(Post $post)
     {
         return view('posts.show', [
@@ -26,15 +14,13 @@ class PostsController
         ]);
     }
 
-    public function ogImage(Post $post)
+    public function redirectOldPostsIndex(Sheets $sheets)
     {
-        return view('posts.ogImage', [
-            'post' => $post,
-        ]);
+        return redirect()->action([HomeController::class, 'index']);
     }
 
     public function redirectOldPost(string $year, string $slug)
     {
-        return redirect()->action('PostsController@show', $slug);
+        return redirect()->action([self::class, 'show'], $slug);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::view('about', 'about.index')->name('about');
 
 Route::view('privacy', 'privacy.index')->name('privacy');
 
-Route::get('posts', 'PostsController@index')->name('posts');
+Route::get('posts', 'PostsController@redirectOldPostsIndex')->name('posts');
 Route::get('posts/{year}/{slug}', 'PostsController@redirectOldPost');
 
 Route::get('{post}', 'PostsController@show')->name('posts.show');
