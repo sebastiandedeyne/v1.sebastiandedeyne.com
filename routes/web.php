c64252c07acf938ce1668c9cf12c9a6f392dcc9e
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-11-26 17:21:58 +0100

    Clean up routes

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,7 @@ Route::view('about', 'about.index')->name('about');
 
 Route::view('privacy', 'privacy.index')->name('privacy');
 
-Route::get('posts', 'PostsController@redirectOldPostsIndex')->name('posts');
+Route::get('posts', 'PostsController@redirectOldPostsIndex');
 Route::get('posts/{year}/{slug}', 'PostsController@redirectOldPost');
 
 Route::get('{post}', 'PostsController@show')->name('posts.show');
-Route::get('{post}/og-image', 'PostsController@ogImage');
-
-Route::domain('growingthestack.io')->group(function () {
-    Route::redirect('{url}', 'sebastiandedeyne.com/newsletter')->where('url', '(.*)');
-});
